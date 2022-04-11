f6187f70e9c792054e999f81767f928803b5668b
unknown <brendon_shif@hotmail.com>
2022-04-11 18:12:21 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Alert from "react-bootstrap/Alert"
 
 import Thumb from "./pokemon-card-photo.js"
 
-import {Link, Outlet} from "react-router-dom"
+import {Outlet} from "react-router-dom"
 
 
 function App() {
